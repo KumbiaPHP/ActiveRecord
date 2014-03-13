215d765d5991a225e9bd70082f59acdc7e50e5e8
manuel_j555 <programador.manuel@gmail.com>
2014-03-12 22:24:25 -0430

    Agregado test de Metadata

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,24 +1,13 @@
 <?php
 
+use Kumbia\ActiveRecord\Autoloader;
+
+require_once __DIR__ . '/../lib/Kumbia/ActiveRecord/Autoloader.php';
+
 define('PRODUCTION', false);
 define('APP_PATH', __DIR__ . '/');
 
-spl_autoload_register(function($className) {
-    $className = ltrim($className, '\\');
-    $fileName = '';
-    $namespace = '';
-    if ($lastNsPos = strripos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-    $folder = dirname(__DIR__) . '/lib/';
-    $file = $folder . DIRECTORY_SEPARATOR . $fileName;
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+Autoloader::register();
 
 class KumbiaException extends Exception
 {
