b5e98afb1efc9faba5377975f3dc7007eeb40527
demonio <distrotuz@gmail.com>
2016-02-22 17:13:47 +0100

    Update SqlsrvMetadata.php
    Divido en 3 el proceso para generar el metadata

--- lib/Kumbia/ActiveRecord/Metadata/SqlsrvMetadata.php
+++ lib/Kumbia/ActiveRecord/Metadata/SqlsrvMetadata.php
@@ -38,25 +38,60 @@ class SqlsrvMetadata extends Metadata
      *
      * @return array
      */
-    protected function queryFields($database, $table, $schema = null)
+    protected function queryFields($database, $table, $schema='dbo')
     {
-        $sql = "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='$table'";
-        $describe = Db::get($database)->query($sql);
-        $fields = array();
+        $describe = Db::get($database)->query("
+            SELECT
+                c.name AS field_name,
+                c.is_identity AS is_auto_increment,
+                c.is_nullable,
+                object_definition(c.default_object_id) AS default_value,
+                t.name AS type_field
+            FROM sys.columns c join sys.types t 
+            ON c.system_type_id = t.user_type_id
+            WHERE object_id = object_id('$schema.$table')
+        ");
+        
+        $pk = self::pk($database, $table);
+    
+        return self::describe($describe, $pk);
+    }
+    
+    /**
+     * Optiene el PK
+     *
+     * @param  string $database base de datos
+     * @param  string $table    tabla
+     * @return string
+     */
+    private static function pk($database, $table)
+    {
         $pk = Db::get($database)->query("exec sp_pkeys @table_name='$table'");
         $pk = $pk->fetch(PDO::FETCH_OBJ);
         $pk = $pk->COLUMN_NAME;
-        // TODO mejorar este código, la consulta SQL no usa el $schema
-        while (( $value = $describe->fetch(PDO::FETCH_OBJ))) :
-            $fields[$value->COLUMN_NAME] = array(
-                'Type' => $value->DATA_TYPE,
-                'Null' => $value->IS_NULLABLE,
-                'Key' => ($value->COLUMN_NAME == $pk) ? 'PRI' : '',
-                'Default' => $value->COLUMN_DEFAULT,
-                'Auto' => ''
+        return $pk;
+    }
+    
+    /**
+     * Genera la metadata
+     *
+     * @param  \PDOStatement $describe
+     * @return array
+     */
+    protected function describe(\PDOStatement $describe, $pk)
+    {
+        $fields = array();
+        while( ( $value = $describe->fetch(PDO::FETCH_OBJ) ) ) :
+            $fields[$value->field_name] = array(
+                'Type' => $value->type_field,
+                'Null' => $value->is_nullable ? 1 : '',
+                'Key' => ($value->field_name == $pk) ? 'PRI' : '',
+                'Default' => str_replace("''", "'", trim($value->default_value, "(')") ),
+                'Auto' => ($value->is_auto_increment) ? 'auto_increment' : ''
             );
-            $this->filterCol($fields[$value->COLUMN_NAME], $value->COLUMN_NAME);
+            $this->filterCol($fields[$value->field_name], $value->field_name);
         endwhile;
+
         return $fields;
     }
 }
