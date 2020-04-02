524463b0ef79aba479825eae2e333d0c75860128
Joanhey <joanhey@kumbiaphp.com>
2020-04-02 13:29:57 +0200

    Fix Mysql create table

--- tests/ActiveRecord/Metadata/MysqlMetadataTest.php
+++ tests/ActiveRecord/Metadata/MysqlMetadataTest.php
@@ -17,7 +17,7 @@ class MysqlMetadataTest extends MetadataTest
     {
         Db::get('mysql')->query('
                 CREATE TABLE IF NOT EXISTS kumbia_test.test ( 
-                    id INT NOT NULL AUTO_INCREMENT, 
+                    id INT(11) NOT NULL AUTO_INCREMENT, 
                     nombre  varchar(50) NOT NULL , 
                     email varchar(100) NOT NULL , 
                     activo smallint(1) NULL DEFAULT 1 , 
