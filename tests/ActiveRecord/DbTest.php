0ab32555fba28a46bcab0c3edb8822a4e1b702a2
Joanhey <joanhey@kumbiaphp.com>
2020-04-02 12:28:40 +0200

    Update DbTests to work without envs

--- tests/ActiveRecord/DbTest.php
+++ tests/ActiveRecord/DbTest.php
@@ -5,19 +5,35 @@ use Kumbia\ActiveRecord\Db;
 
 class DbTest extends TestCase
 {
+    /**
+     * @requires extension pdo_sqlite
+    */
     public function testGetInstance()
     {
-        $instance = Db::get(getenv('DB'));
+        
+        $instance = Db::get('sqlite');
 
         $this->assertInstanceOf('PDO', $instance);
     }
 
+    /**
+     * @requires extension pdo_mysql
+     */
     public function testGet()
     {
-        $instance = Db::get(getenv('DB'));
+        $instance = Db::get('mysql');
 
-        $instance2 = Db::get(getenv('DB'));
+        $instance2 = Db::get('mysql');
 
         $this->assertEquals($instance, $instance2);
+    }
+
+    public function testGetThatDontExistInConfig()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessageRegExp('/No existen datos de conexión para la bd$/');
+        
+        $instance = Db::get('no_exist');
+
     }
 }
