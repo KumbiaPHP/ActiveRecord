06fba399cb32c23ae28d731c9ce0f39ebc0744c4
Henry Stivens Adarme <henry.stivens@gmail.com>
2020-05-10 20:09:33 -0500

    Added sqlite last insert id and limit functions

---
+++ Query/sqlite_limit.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * KumbiaPHP web & app Framework.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://wiki.kumbiaphp.com/Licencia
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@kumbiaphp.com so we can send you a copy immediately.
+ *
+ * @category   Kumbia
+ *
+ * @copyright  2005 - 2020  Kumbia Team (http://www.kumbiaphp.com)
+ * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
+ */
+
+namespace Kumbia\ActiveRecord\Query;
+
+/**
+ * Adiciona limit y offset a la consulta sql en sqlite.
+ *
+ * @param  string   $sql    consulta select
+ * @param  int      $limit  valor limit
+ * @param  int      $offset valor offset
+ * @return string
+ */
+function sqlite_limit($sql, int $limit, int $offset = 0)
+{
+    return $sql." LIMIT $offset, $limit";
+}
